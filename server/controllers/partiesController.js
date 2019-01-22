59f3f84dd23061fd8e5d355b7beceed747180fda
Idowu Wasiu <hoxtygen@live.com>
2019-01-22 23:38:35 +0100

    ft(feature):Get a single political party
    ensure user can get one political party
    
    [Starts ft-get-one-party-#163395830]

--- server/controllers/partiesController.js
+++ server/controllers/partiesController.js
@@ -7,6 +7,20 @@ class partyController {
       data: Parties,
     });
   }
+
+  static getOneParty(req, res) {
+      const getParty = Parties.find(party => party.id === parseInt(req.params.id, 10));
+      if(getParty) {
+          res.status(200).json({
+              status: 200,
+              data: getParty,
+          })
+      }
+      return res.status(404).json({
+          status: 404,
+          error: 'Party does not exist',
+      })
+  }
 }
 
 export default partyController;
--- server/routes/partiesRoute.js
+++ server/routes/partiesRoute.js
@@ -3,5 +3,6 @@ import partiesController from '../controllers/partiesController';
 
 const partiesRouter = Router();
 partiesRouter.get('/parties', partiesController.getAllParties);
+partiesRouter.get('/parties/:id', partiesController.getOneParty);
 
 export default partiesRouter;
