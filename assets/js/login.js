f7ae40c74e4fd08cacefb3a9175e180b291a6d0c
Idowu Wasiu <hoxtygen@live.com>
2019-01-26 23:28:03 +0100

    ch(css styles): improve landing page
    The landing page made a bit better by making it less blank and adding
    more content. Also, a login link was added to all the pages so users
    could log out of the application. In addition, a login script was added
    to control the flow of users on signing in - An admin will be redirected
    to admin dashboard with more functionalities while non-admin users will
    be redirected to dashboards with minimal functionalites.

---
+++ assets/js/login.js
@@ -0,0 +1,39 @@
+const Users = [
+    {
+        username: 'Admin@yahoo.com',
+        password: 'admin123',
+        isAdmin: true
+    },
+
+    {
+        username: 'user1@yahoo.com',
+        password: 'user123',
+        isAdmin: false
+    },
+
+    {
+        username: 'user2@yahoo.com',
+        password: 'user123',
+        isAdmin: false
+    },
+];
+
+
+
+function logUser() {
+    const username = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    /* console.log(`Your username is ${username} and password is ${password}`); */
+    for (let i = 0; i < Users.length; i++) {
+        if (username === Users[i].username && password === Users[i].password && Users[i].isAdmin === true) {
+            console.log('You are now logged in as an admin')
+            window.location = 'dashboard.html';
+            return;
+        } else if (username === Users[i].username && password === Users[i].password && Users[i].isAdmin === false) {
+            window.location = 'contestants.html';
+            return
+        }
+    }
+    window.location = 'index.html';
+    //console.log('you are not an admin');
+}
