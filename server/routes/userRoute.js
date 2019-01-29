a1166f78a3ba2ef91e2449ee99e14ef3dbb7809d
Idowu Wasiu <hoxtygen@live.com>
2019-01-29 02:04:16 +0100

    ft(feature): login user
    This route enables registered users to login to their account and
    perform activities such as view their profile, vote, contest for
    election.....etc. Because most of the routes are protected, a token is
    generated if the user supplies a valid email and password, otherwise a
    user is denied access into the application.
    
    [Delivers #163541092]

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import verify from '../controllers/verifyToken';
 import UserController from '../controllers/userController';
 
 const usersRouter = Router();
 
-usersRouter.get('/users', UserController.getAllUsers);
+usersRouter.get('/users', verify.verifyToken, UserController.getAllUsers);
 usersRouter.post('/signup', UserController.addNewUser);
+usersRouter.post('/login', UserController.login);
 
 export default usersRouter;
