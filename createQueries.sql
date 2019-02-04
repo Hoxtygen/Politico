387c2886a7dc9bf02ac5209af97f9083844a1c1f
Idowu Wasiu <hoxtygen@live.com>
2019-02-04 07:12:01 +0100

    create heroku postgresql tables

--- createQueries.sql
+++ createQueries.sql
@@ -1,23 +1,3 @@
-\c politico hoxtygen
+INSERT INTO politico_andela.Users (firstName, lastName, otherName, email, phoneNumber, passportUrl, isAdmin, password) VALUES ('Wasiu', 'Idowu', 'Adeniyi', 'adenwas@gmail.com', '08060184972', 'https://www.adenpassport.com', 'true', 'shaolindragon');
 
 
-CREATE TABLE IF NOT EXISTS politico_andela.candidates (
-    id SERIAL  NOT NULL,
-    office INTEGER REFERENCES politico_andela.offices(id) NOT NULL,
-    candidate INTEGER REFERENCES politico_andela.users(id) NOT NULL,
-    FOREIGN KEY(office) REFERENCES politico_andela.offices(id),
-    FOREIGN KEY(candidate) REFERENCES politico_andela.users(id),
-    PRIMARY KEY (office, candidate)
-);
-
-
-CREATE TABLE IF NOT EXISTS politico_andela.votes (
-    id SERIAL NOT NULL,
-    createdOn DATE NOT NULL DEFAULT CURRENT_DATE,
-    voter INT REFERENCES politico_andela.users(id) NOT NULL,
-    office INTEGER REFERENCES politico_andela.offices(id) NOT NULL,
-    candidate INTEGER REFERENCES politico_andela.users(id) NOT NULL,
-    FOREIGN key(voter) REFERENCES politico_andela.users(id),
-    FOREIGN KEY(candidate) REFERENCES politico_andela.users(id),
-    PRIMARY KEY (office, voter)
-);
--- queryStorge.sql
+++ queryStorge.sql
@@ -78,3 +78,24 @@ CREATE TABLE IF NOT EXISTS politico_andela.votes (
 );
 
 INSERT INTO politico_andela.votes (office, voter,candidate) VALUES (1, 1, 1);
+
+CREATE TABLE IF NOT EXISTS politico_andela.candidates (
+    id SERIAL  NOT NULL,
+    office INTEGER REFERENCES politico_andela.offices(id) NOT NULL,
+    candidate INTEGER REFERENCES politico_andela.users(id) NOT NULL,
+    FOREIGN KEY(office) REFERENCES politico_andela.offices(id),
+    FOREIGN KEY(candidate) REFERENCES politico_andela.users(id),
+    PRIMARY KEY (office, candidate)
+);
+
+
+CREATE TABLE IF NOT EXISTS politico_andela.votes (
+    id SERIAL NOT NULL,
+    createdOn DATE NOT NULL DEFAULT CURRENT_DATE,
+    voter INT REFERENCES politico_andela.users(id) NOT NULL,
+    office INTEGER REFERENCES politico_andela.offices(id) NOT NULL,
+    candidate INTEGER REFERENCES politico_andela.users(id) NOT NULL,
+    FOREIGN key(voter) REFERENCES politico_andela.users(id),
+    FOREIGN KEY(candidate) REFERENCES politico_andela.users(id),
+    PRIMARY KEY (office, voter)
+);
